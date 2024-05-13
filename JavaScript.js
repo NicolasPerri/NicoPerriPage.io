42790c4b2fa7d0d482a1ca34b6e10f8587d7d3b1
diegostampa <163307116+diegostampa@users.noreply.github.com>
2024-05-13 19:05:04 -0300

    JS

---
+++ JavaScript.js
@@ -0,0 +1,13 @@
+const form = document.querySelector('form');
+const nameInput = document.querySelector('#name');
+const mailInput = document.querySelector('#mail');
+const msgInput = document.querySelector('#msg');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  if (nameInput.validity.valid && mailInput.validity.valid && msgInput.validity.valid) {
+    alert('Formulario válido!');
+  } else {
+    alert('Por favor, complete los campos correctamente.');
+  }
+});
