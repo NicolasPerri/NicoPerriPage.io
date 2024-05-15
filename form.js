6f9665a9d3197a4a2b8832e25e323ecbea93a9bb
diegostampa <163307116+diegostampa@users.noreply.github.com>
2024-05-15 16:51:53 -0300

    vinculando js

--- form.js
+++ form.js
@@ -12,3 +12,22 @@ form.addEventListener('submit', (e) => {
   }
 });
 
+//* Dealing with Input width
+let el = document.querySelector(".input-wrap .input");
+let widthMachine = document.querySelector(".input-wrap .width-machine");
+el.addEventListener("keyup", () => {
+  widthMachine.innerHTML = el.value;
+});
+
+// Dealing with Textarea Height
+function calcHeight(value) {
+  let numberOfLineBreaks = (value.match(/\n/g) || []).length;
+  // min-height + lines x line-height + padding + border
+  let newHeight = 20 + numberOfLineBreaks * 20 + 12 + 2;
+  return newHeight;
+}
+
+let textarea = document.querySelector(".comment");
+textarea.addEventListener("keyup", () => {
+  textarea.style.height = calcHeight(textarea.value) + "px";
+});
